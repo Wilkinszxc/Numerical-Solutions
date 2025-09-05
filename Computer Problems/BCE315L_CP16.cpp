753883fe05c9829f53e4369311f2373e8a310803
WilBurgz <wilkinsburgz99@gmail.com>
2025-09-05 18:06:39 +0800

    Done calculations, redoing this for tabulation and better results tabulation

--- Computer Problems/BCE315L_CP16.cpp
+++ Computer Problems/BCE315L_CP16.cpp
@@ -9,13 +9,38 @@ COORD CursorPosition;
 int gotoxy(int x, int y);
 
 int main(){
-   
+    int integers[10];
+    int sum = 0, largest, smallest;
+
+    cout << "Name: AL WILKINS BURGOS\n";
+    cout << "BCE 315L (420)\n";
+    cout << "CP13 - 10 integer's Average, Largest, Smallest Number\n\n";
+
+    for(int i = 0; i < 10; i++){
+        cout << "Enter integer " << (i + 1) << ": ";
+        cin >> integers[i];
+        sum += integers[i];
+
+        if(i == 0){
+            largest = smallest = integers[i];
+        } else {
+            if(integers[i] > largest) largest = integers[i];
+            if(integers[i] < smallest) smallest = integers[i];
+        }
+    }
+
+    double average = sum / 10.0;
+
+    cout << "\nAverage: " << average << endl;
+    cout << "Largest: " << largest << endl;
+    cout << "Smallest: " << smallest << endl;
+
 
 
     cout << endl << endl;
     system("pause");
- 
 }
+
 
 int gotoxy(int x, int y){
 	CursorPosition.X = x;
