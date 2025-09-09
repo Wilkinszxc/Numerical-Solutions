1a2e3658c7a85dfa52d8d369b329fd4fa5144657
WilBurgz <wilkinsburgz99@gmail.com>
2025-09-09 17:05:54 +0800

    FIXED ISTG!

--- Computer Problems/BCE315L_CP16.cpp
+++ Computer Problems/BCE315L_CP16.cpp
@@ -10,69 +10,68 @@ COORD CursorPosition;
 int gotoxy(int x, int y);
 
 int main(){
-    int integers[10];
-    int sum = 0, largest, smallest;
     char choice;
+    do {
+        int integers[10];
+        int sum = 0, largest, smallest;
 
-    cout << "Name: AL WILKINS BURGOS\n";
-    cout << "BCE 315L (420)\n";
-    cout << "CP13 - 10 integer's Average, Largest, Smallest Number\n\n";
+        cout << "Name: AL WILKINS BURGOS\n";
+        cout << "BCE 315L (420)\n";
+        cout << "CP13 - 10 integer's Average, Largest, Smallest Number\n\n";
 
-    for(int i = 0; i < 10; i++){
-        cout << "Enter integer " << (i + 1) << ": ";
-        cin >> integers[i];
-        sum += integers[i];
+        for(int i = 0; i < 10; i++){
+            cout << "Enter integer " << (i + 1) << ": ";
+            cin >> integers[i];
+            sum += integers[i];
 
-        if(i == 0){
-            largest = smallest = integers[i];
-        } else {
-            if(integers[i] > largest) largest = integers[i];
-            if(integers[i] < smallest) smallest = integers[i];
+            if(i == 0){
+                largest = smallest = integers[i];
+            } else {
+                if(integers[i] > largest) largest = integers[i];
+                if(integers[i] < smallest) smallest = integers[i];
+            }
         }
-    }
 
-    double average = sum / 10.0;
+        double average = sum / 10.0;
 
-    gotoxy( 30, 5);
-    cout << "+--------------+--------------+--------------+";
-    gotoxy(30, 6);
-    cout << "|    Average   |    Largest   |   Smallest   |";
-    gotoxy(30, 7);
-    cout << "+--------------+--------------+--------------+";
-    gotoxy(30, 8);
-    cout << "|              |              |              |";
-    gotoxy(30, 9);
-    cout << "|              |              |              |";
-    gotoxy(30, 10);
-    cout << "|              |              |              |";
-    gotoxy( 30, 11);
-    cout << "+--------------+--------------+--------------+";
-    
-    gotoxy(36, 9); 
-    cout << average;
-    gotoxy(52, 9);
-    cout << largest;
-    gotoxy(68, 9);
-    cout << smallest;
+        gotoxy( 30, 5);
+        cout << "+--------------+--------------+--------------+";
+        gotoxy(30, 6);
+        cout << "|    Average   |    Largest   |   Smallest   |";
+        gotoxy(30, 7);
+        cout << "+--------------+--------------+--------------+";
+        gotoxy(30, 8);
+        cout << "|              |              |              |";
+        gotoxy(30, 9);
+        cout << "|              |              |              |";
+        gotoxy(30, 10);
+        cout << "|              |              |              |";
+        gotoxy( 30, 11);
+        cout << "+--------------+--------------+--------------+";
+        
+        gotoxy(36, 9); 
+        cout << average;
+        gotoxy(52, 9);
+        cout << largest;
+        gotoxy(68, 9);
+        cout << smallest;
 
-    gotoxy(0, 18);
-    cout << "Do you want to redo calculations? (y/n): ";
-    cin >> choice;
+        gotoxy(0, 18);
+        cout << "Do you want to redo calculations? (y/n): ";
+        cin >> choice;
 
-    if(choice == 'N' || choice == 'n'){
-        system("pause");
-        return 0;
-    }else {
-        system("cls");
-        main();
-    }
+        if(choice == 'Y' || choice == 'y'){
+            system("cls");
+        }
+    } while(choice == 'Y' || choice == 'y');
 
+    system("pause");
     return 0;
 }
-
 
 int gotoxy(int x, int y){
     CursorPosition.X = x;
     CursorPosition.Y = y;
     SetConsoleCursorPosition(console, CursorPosition);
+    return 0;
 }
